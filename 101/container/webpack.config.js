98ab614bb008fddfcdfc017cf870a3b21426ce81
MartinGrue <gruebler.martin@outlook.de>
2020-11-18 17:29:05 +0100

    module federation working

---
+++ 101/container/webpack.config.js
@@ -0,0 +1,14 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: { port: 1080 },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: { products: "products@http://localhost:1081/remoteEntry.js" },
+    }),
+    new htmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+};
---
+++ 101/products/webpack.config.js
@@ -0,0 +1,14 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+module.exports = {
+  mode: "development",
+  devServer: { port: 1081 },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: { "./ProductsIndex": "./src/index" },
+    }),
+    new htmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
+};
