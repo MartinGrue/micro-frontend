95eb9e18f62a11ee3a3d80a55d8c57b70196ac80
MartinGrue <gruebler.martin@outlook.de>
2020-11-29 16:02:42 +0100

    test

--- packages/container/paths.js
+++ packages/container/paths.js
@@ -1,6 +1,7 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  src: path.resolve(__dirname, '/src'),
-  build: path.resolve(__dirname, 'dist'),
-}
+  src: path.resolve(__dirname, "/src"),
+  build: path.resolve(__dirname, "dist"),
+};
+console.log("test run action");
