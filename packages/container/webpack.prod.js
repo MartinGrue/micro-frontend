91140d512ed2ab6b0eab5e1259f3c6439fbf9657
MartinGrue <gruebler.martin@outlook.de>
2020-11-30 13:57:16 +0100

    23

--- packages/container/webpack.prod.js
+++ packages/container/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
   output: {
     path: paths.build,
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/",
+    publicPath: "/marketing/latest/",
   },
 
   devServer: {
