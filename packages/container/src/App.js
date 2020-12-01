93ddd75a59bd81292b088103fe32b9203090df89
MartinGrue <gruebler.martin@outlook.de>
2020-12-01 14:04:26 +0100

    added packaged specific css class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,24 @@ import React from "react";
 import MerketingApp from "./components/MerketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header></Header>
-        <h1>Hi There</h1>
-        <hr></hr>
-        <MerketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header></Header>
+          <h1>Hi There</h1>
+          <hr></hr>
+          <MerketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { Switch, Route,  BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
-} from '@material-ui/core/styles';
+} from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
+//this is needed to avoid css class name collision
+//with other projectes that are using mateial-ui
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma',
+  productionPrefix: "ma",
 });
 
 export default () => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter >
+        <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
